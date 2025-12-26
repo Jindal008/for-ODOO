02bb864b79564eded1da46e97239d3b5aa79aff7
Jindal008 <jindalpatel06890@gmail.com>
2025-12-26 22:21:05 +0530

    first

--- my-next-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-   <div className="bg-red-200">
-      <h1 className="font-bold text-3xl">FOR - ODOO</h1>
-   </div>
-  );
-}
